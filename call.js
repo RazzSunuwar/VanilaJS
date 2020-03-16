0955353d9847e3ffe66ad68c76094fdf3a961b04
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-16 21:50:07 +0545

    bind

--- call.js
+++ call.js
@@ -6,5 +6,8 @@ let addToThis = function(a, b, c){
 
 // console.log(addToThis.call(obj, 3));    // functionname.call(obj, functionarguments)
 
-let arr = [1,2,3];
-console.log(addToThis.apply(obj, arr));
+// let arr = [1,2,3];
+// console.log(addToThis.apply(obj, arr));
+
+var bound = addToThis.bind(obj);
+console.log(bound(1,2,3));
