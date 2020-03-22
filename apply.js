0c1aac5630b63fc77f388eb76bbb38a757434e28
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-22 21:42:44 +0545

    Apply function

--- apply.js
+++ apply.js
@@ -1,20 +1,42 @@
 // Apply function
+//
+// let Person = {
+//     fullName: function(address, phone) {
+//         return this.firstName+ " "+ this.lastName+", "+ address+", "+ phone;
+//     }
+// };
+//
+// let PersonFirst = {
+//     firstName: "Ronald",
+//     lastName: "Gate"
+// };
+//
+// let PersonSecond = {
+//     firstName: "Romeo",
+//     lastName: "Gate"
+// };
+//
+// personFullName = Person.fullName.apply(PersonFirst, ["Ktm", "124555"]);
+// console.log(personFullName);
+
+
+// Apply function
 
 let Person = {
-    fullName: function(address, phone) {
-        return this.firstName+ " "+ this.lastName+", "+ address+", "+ phone;
-    }
-};
+  fullName: function() {
+    return this.firstName+ " "+ this.lastName;
+  }
+}
 
-let PersonFirst = {
-    firstName: "Ronald",
-    lastName: "Gate"
-};
+let PersonFirst ={
+  firstName: 'John',
+  lastName: 'Doe'
+}
 
 let PersonSecond = {
-    firstName: "Romeo",
-    lastName: "Gate"
-};
+  firstName: 'Albert',
+  lastName: 'Doe'
+}
 
-personFullName = Person.fullName.apply(PersonFirst, ["Ktm", "124555"]);
-console.log(personFullName);
+PersonFullName = Person.fullName.apply(PersonFirst);
+console.log(PersonFullName);
