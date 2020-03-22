56b63bffd2748c7d3d6f11524e5d62d8e71c3a1f
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-22 22:28:17 +0545

    Bind function Practice

--- bind.js
+++ bind.js
@@ -1,15 +1,31 @@
-// bind function
+// // bind function
+//
+// let Person = {
+//     name: "John"
+// };
+//
+// let PersonGreets = function(a){
+//     return "Welcome "+ this.name+" "+"to"+" "+ a;
+// };
+//
+// let bound = PersonGreets.bind(Person); //Create a bound function that has same body and parameters
+//
+// console.dir(bound);     //returns a function
+//
+// console.log(bound("KTM"));
+
+
+// Bind Practice
 
 let Person = {
-    name: "John"
-}; 
-
-let PersonGreets = function(a){
-    return "Welcome "+ this.name+" "+"to"+" "+ a;
+  firstName: "Daniel",
+  lastName: "Dyhl"
 };
 
-let bound = PersonGreets.bind(Person); //Create a bound function that has same body and parameters
+let PersonFunction = function(a) {
+  return "Hello"+" "+this.firstName+" "+"Welcome"+" "+"to "+a;
+};
 
-console.dir(bound);     //returns a function
-
-console.log(bound("KTM"));
+let bound = PersonFunction.bind(Person);
+console.dir(bound);
+console.log(bound("Nepal"));
