6a89a9f0008c0e9f341f41c25ae42a440c6b738d
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-24 20:03:31 +0545

    Practice Callback

--- callback.js
+++ callback.js
@@ -1,11 +1,25 @@
 // Callbacks
-function myplan(){
-    console.log('I want to buy motorbike');
-    console.log('I asked money with parents');
-    console.log('My parents told me to wait for the money');
-    setTimeout(function(){
-        console.log('Salary arrived');
-        console.log('Now i am going to buy motorbike');
-    }, 2000);
+
+// Practice 1
+// function myplan(){
+//     console.log('I want to buy motorbike');
+//     console.log('I asked money with parents');
+//     console.log('My parents told me to wait for the money');
+//     setTimeout(function(){
+//         console.log('Salary arrived');
+//         console.log('Now i am going to buy motorbike');
+//     }, 2000);
+// }
+// myplan();
+
+
+//Practice 2
+function buyMobile(model){
+    return model;
 }
-myplan();
+
+console.log('I want to buy mobile');
+let mobile = buyMobile('S10');
+console.log('I want to buy >>', mobile);
+console.log('Take photos');
+console.log('Post it in the Instagram');
