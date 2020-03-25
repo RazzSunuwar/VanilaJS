653814b8d3f7ce9cd030ea9ad7cc975cddf6ba4f
RazzSunuwar <razzkoinch5@gmail.com>
2020-03-25 20:29:53 +0545

    callback

--- callback.js
+++ callback.js
@@ -26,26 +26,59 @@
 
 
 
-//Task Part
-function askMoney(callback){
-    console.log('Mom told me to wait for the money untill salary arrived');
+// Practice 3
+
+// //Task Part
+// function askMoney(callback){
+//     console.log('Mom told me to wait for the money untill salary arrived');
+//     setTimeout(function(){
+//         console.log('Salary Arrived!');
+//         callback();
+//     }, 2000);
+// }
+
+
+// // Execution part
+// console.log('I want to buy mobile');
+// console.log(`I don't have money so i asked to my parents`);
+// askMoney(function(){
+//     console.log('Asked money arrived');
+//     console.log('Now i have to perform all the works which needs money');
+// });
+// console.log('Eat foods');
+// console.log('Do exercise');
+// console.log('Eat Again eat foods');
+
+
+// Practice 4
+
+// Task Part
+function buyPhone(cb){
     setTimeout(function(){
-        console.log('Salary Arrived!');
-        callback();
+        console.log('I am buying phone');
+        cb();
     }, 2000);
 }
 
+function takePhoto(){
+    console.log('Taking photos');
+};
 
-// Execution part
-console.log('I want to buy mobile');
-console.log(`I don't have money so i asked to my parents`);
-askMoney(function(){
-    console.log('Asked money arrived');
-    console.log('Now i have to perform all the works which needs money');
+function postInInstagram(){
+    console.log('Post it in the instagram');
+};
+
+
+// Execution Part
+buyPhone(function(){
+    takePhoto();
+    postInInstagram();
 });
-console.log('Eat foods');
-console.log('Do exercise');
-console.log('Eat Again eat foods');
+console.log('Its raining');
+
+
+
+
 
 
 
