3040370fa86062952e1791f06fa9dc7de45645c2
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-17 21:50:16 +0545

    Promise

--- PromisePractice.js
+++ PromisePractice.js
@@ -23,26 +23,67 @@
 //   });
 
 
-  // Task part
-  function PrintNote(pdfNote){
-    return new Promise(function(resolve, reject){
-      console.log("Note is in the stationary");
-      console.log("Shopkeeper promise to call me after finished printing");
-      setTimeout(function(){
-        let electricity = true;
-        if(electricity){
-          resolve("Success")
-        }else{
-          reject('Error')
-        }
-      }, 2000);
-    })
-  };
+  // // Task part
+  // function PrintNote(pdfNote){
+  //   return new Promise(function(resolve, reject){
+  //     console.log("Note is in the stationary");
+  //     console.log("Shopkeeper promise to call me after finished printing");
+  //     setTimeout(function(){
+  //       let electricity = true;
+  //       if(electricity){
+  //         resolve("Success")
+  //       }else{
+  //         reject('Error')
+  //       }
+  //     }, 2000);
+  //   })
+  // };
+  //
+  // PrintNote("PdfNote")
+  // .then(function(data){
+  //   console.log("Print note >>>", data);
+  // })
+  // .catch(function(error){
+  //   console.log("Print note >>>", error);
+  // })
 
-  PrintNote("PdfNote")
-  .then(function(data){
-    console.log("Print note >>>", data);
+// // Task part
+// function entertainment(){
+//   return new Promise(function(resolve, reject){
+//     console.log("Entertainment start");
+//     setTimeout(function(){
+//       resolve()
+//     }, 2000)
+//   })
+// }
+//
+// // Execution Part
+// entertainment()
+// .then(function(){
+//   console.log("Entertainment finished. It was Nice movie")
+// })
+// .catch(function(){
+//
+// })
+
+
+// Task part
+function servicing(){
+  return new Promise(function(resolve, reject){
+    console.log("Bike is at servicing");
+    setTimeout(function(){
+      resolve()
+    }, 2000)
   })
-  .catch(function(error){
-    console.log("Print note >>>", error);
-  })
+}
+
+// Execution Part
+servicing()
+.then(function(){
+  console.log("Servicing Finished")
+  console.log("Now start Preparation")
+
+})
+.catch(function(){
+
+})
