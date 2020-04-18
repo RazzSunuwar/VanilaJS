85c70ae41379adf786f263047c49f823d026dd6a
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-18 20:36:58 +0545

    Simple callback

--- callback.js
+++ callback.js
@@ -89,17 +89,46 @@
 
 // Practice 5
 
-let x = function(){
-  console.log("i am call from inside a function")
-}
-
-let y = function(callback){
-  console.log("do something")
-  callback()
-}
-
-y(x);
+// let x = function(){
+//   console.log("i am call from inside a function")
+// }
+//
+// let y = function(callback){
+//   console.log("do something")
+//   callback()
+// }
+//
+// y(x);
 
 // Expected result
 // do something
 // i am call from inside a function
+
+
+
+// Practice 6
+// let calc = function(num1, num2, calcType){
+//   if (calcType === "add"){
+//     return num1 + num2;
+//   } else if (calcType === "multiply") {
+//     return num1 * num2;
+//   }
+// }
+//
+// console.log(calc(2, 3, "multiply"));
+
+
+//                        OR
+let add = function(a,b){
+  return a + b;
+}
+
+let multiply = function(a,b){
+  return a*b;
+}
+
+let calc = function(num1, num2, callback){
+  return callback(num1, num2);
+}
+
+console.log(calc(2, 3, multiply));
