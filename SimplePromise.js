fc148291666bd34190ede5718e1502396ff3c946
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-18 15:44:04 +0545

    Promise

--- SimplePromise.js
+++ SimplePromise.js
@@ -1,17 +1,36 @@
-let p = new Promise((resolve, reject) => {
-  let a = 2+5;
-  if(a==5) {
-    resolve("Success")
-  }else {
+// let p = new Promise((resolve, reject) => {
+//   let a = 2+5;
+//   if(a==5) {
+//     resolve("Success")
+//   }else {
+//     reject("Failure")
+//   }
+//
+// });
+//
+// p
+// .then(function(data){
+//   console.log("This is ", data);
+// })
+// .catch(function(err){
+//   console.log("This is ", err);
+// })
+
+
+
+const hack = new Promise(function(resolve, reject){
+  let hacker = true;
+  if(hacker){
+    resolve("Succeful")
+  }else{
     reject("Failure")
   }
-
-});
-
-p
-.then(function(data){
-  console.log("This is ", data);
 })
-.catch(function(err){
-  console.log("This is ", err);
+
+hack
+.then(function(data){
+  console.log("Hacking ", data)
+})
+.catch(function(data){
+  console.log("Hacking", data)
 })
