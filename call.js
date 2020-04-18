00677fb553ca9881b8deb8c606df1349b55a8d92
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-18 21:08:10 +0545

    call method

--- call.js
+++ call.js
@@ -1,16 +1,16 @@
-// let obj = {num:2};
+let obj = {num:2};
 
-// let addToThis = function(a, b, c){
-//     return this.num + a + b + c;
-// };
+let addToThis = function(a, b, c){
+    return this.num + a + b + c;
+};
 
-// // console.log(addToThis.call(obj, 3));    // functionname.call(obj, functionarguments)
+// console.log(addToThis.call(obj, 3));    // functionname.call(obj, functionarguments)
 
-// // let arr = [1,2,3];
-// // console.log(addToThis.apply(obj, arr));
+// let arr = [1,2,3];
+// console.log(addToThis.apply(obj, arr));
 
-// var bound = addToThis.bind(obj);
-// console.log(bound(1,2,3));
+var bound = addToThis.bind(obj);
+console.log(bound(1,2,3));
 
 
 // Call Function
