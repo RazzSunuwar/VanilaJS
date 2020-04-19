7b4db296a1f0796d13ce94200422369fcfc70838
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-19 14:50:17 +0545

    Promise practice FreeCodeCamp

---
+++ PromiseFreeCodeCamp.js
@@ -0,0 +1,47 @@
+let add = function(x,y) {
+  return new Promise((resolve, reject) => {
+    let sum = x+y;
+    if(sum) {
+      resolve(sum);
+    }else{
+      reject(Error("Could not add the two values !"));
+    }
+  });
+};
+
+let subtract = function(x,y) {
+  return new Promise((resolve, reject) => {
+    let sum = x -y;
+    if (sum) {
+      resolve(sum);
+    } else {
+      reject(Error("Could not subtract the two values !"));
+    }
+  });
+};
+
+// Starting promise chain
+add(2,2)
+.then((added) => {
+  // added = 4
+  return subtract(added, 3)
+})
+
+.then((subtracted) => {
+  // added = 1
+  return add(subtracted, 5)
+})
+
+.then((added) => {
+  // added = 6
+  return added * 2;
+})
+
+.then((result) => {
+  // resul= 12
+  console.log("My result is ", result);
+})
+
+.catch((err) => {
+  consolle.log(err);
+})
