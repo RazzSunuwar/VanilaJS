c209c522aee5e7eb41895313d5557ad2ec3e47ce
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-21 19:01:15 +0545

    First Async await function

--- AsyncAwait2.js
+++ AsyncAwait2.js
@@ -1,9 +1,29 @@
+// console.log("Person 1: Show ticket");
+// console.log("Person 2: Show ticket");
+//
+// const preMovie = async () => 'preMovie';
+// preMovie()
+// .then((m) => console.log(m));
+//
+// console.log("Person 4: Show ticket");
+// console.log("Person 5: Show ticket");
+
+
+
 console.log("Person 1: Show ticket");
 console.log("Person 2: Show ticket");
 
-const preMovie = async () => 'preMovie';
-preMovie()
-.then((m) => console.log(m));
+const preMovie = async () => {
+
+  const promsieWifeBringingTicks = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('ticket'), 3000);
+  });
+
+  let ticket = await promsieWifeBringingTicks;
+
+  return ticket;
+}
+preMovie().then((m) => console.log(m));
 
 console.log("Person 4: Show ticket");
 console.log("Person 5: Show ticket");
