679853e7d1deb549e3dfc61b622e3e4927a1e921
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-21 18:25:09 +0545

    Simple AsyncAwait Practice

---
+++ AsyncAwait2.js
@@ -0,0 +1,9 @@
+console.log("Person 1: Show ticket");
+console.log("Person 2: Show ticket");
+
+const preMovie = async () => 'preMovie';
+preMovie()
+.then((m) => console.log(m));
+
+console.log("Person 4: Show ticket");
+console.log("Person 5: Show ticket");
