d6a045de6d22462a34e0ef12b13b215817f4661e
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-21 22:36:07 +0545

    Asyn await

--- AsyncAwait3.js
+++ AsyncAwait3.js
@@ -1,3 +1,37 @@
+console.log("Person 1: Shows ticket");
+console.log("Person 2: Shows ticket");
+
+const preMovie = async () => {
+
+  const promsieWifeBringingTicks = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('ticket'), 3000);
+  });
+
+  const getPopcorn = new Promise((resolve, reject) => resolve(`Popcorn`));
+
+  const getCandy = new Promise((resolve, reject) => resolve(`candy`));
+
+  const getCoke = new Promise((resolve, reject) => resolve(`coke`));
+
+  let ticket = await promsieWifeBringingTicks;
+
+  let [popcorn, candy, coke] = await Promise.all([getPopcorn,getCandy,getCoke]);
+
+  console.log(`${popcorn}, ${candy}, ${coke}`)
+
+  return ticket;
+}
+
+preMovie().then((m) => console.log(`Person 3: Shows ${m}`));
+
+console.log("Person 4: Shows ticket");
+console.log("Person 5: Shows ticket");
+
+
+
+
+// ASYNC AWAIT AND TRY CATCH
+
 // console.log("Person 1: Shows ticket");
 // console.log("Person 2: Shows ticket");
 //
@@ -7,17 +41,12 @@
 //     setTimeout(() => resolve('ticket'), 3000);
 //   });
 //
-//   const getPopcorn = new Promise((resolve, reject) => resolve(`Popcorn`));
-//
-//   const getCandy = new Promise((resolve, reject) => resolve(`candy`));
-//
-//   const getCoke = new Promise((resolve, reject) => resolve(`coke`));
-//
-//   let ticket = await promsieWifeBringingTicks;
-//
-//   let [popcorn, candy, coke] = await Promise.all([getPopcorn,getCandy,getCoke]);
-//
-//   console.log(`${popcorn}, ${candy}, ${coke}`)
+//   let ticket
+//   try {
+//     ticket = await promsieWifeBringingTicks;
+//   } catch(err){
+//     ticket = 'sad face';
+//   }
 //
 //   return ticket;
 // }
@@ -30,31 +59,28 @@
 
 
 
-// ASYNC AWAIT AND TRY CATCH
-
-console.log("Person 1: Shows ticket");
-console.log("Person 2: Shows ticket");
-
-const preMovie = async () => {
-
-  const promsieWifeBringingTicks = new Promise((resolve, reject) => {
-    setTimeout(() => reject('ticket'), 3000);
-  });
-
-  let ticket
-  try {
-    ticket = await promsieWifeBringingTicks;
-  } catch(err){
-    ticket = 'sad face';
-  }
 
 
+// Test User Form
 
-
-  return ticket;
-}
-
-preMovie().then((m) => console.log(`Person 3: Shows ${m}`));
-
-console.log("Person 4: Shows ticket");
-console.log("Person 5: Shows ticket");
+// const testUserForm = async () => {
+//
+//   const loadUserForm = new Promise((resolve, reject) => {
+//     setTimeout(()=> resolve('page loaded'), 3000);
+//   });
+//
+//   const enterUserName = new Promise((resolve, reject) => {
+//     setTimeout(()=> resolve('enter username'), 3000);
+//   });
+//
+//   const verifyUserDetails = () => {
+//
+//   }
+//
+//   await loadUserForm;
+//   await enterUserName;
+//   const testResult = verifyUserDetails();
+//
+//   return testResult;
+// }
+// testUserForm().then((m) => console.log(`Shows ${m}`));
