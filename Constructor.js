332daa601adc63f2812f3066fb04345688f762c1
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-23 19:35:39 +0545

    Constructor

---
+++ Constructor.js
@@ -0,0 +1,23 @@
+/* Constructor: Object instance are created with constructor, which are basically special function that prepare new
+instance of an object for use.
+*/
+
+// function Mobile() {
+// 	this.model = '3310';
+// 	this.price = function(){
+// 	document.write(this.model + " Price Rs. 3000 ");
+// 	}
+// }
+// let Samsung = new Mobile();
+// let OnePlus = new Mobile(); //New Instance created with name One Plus
+// Samsung.price();
+// OnePlus.price();
+
+// this key will point towards new instance or 'samsung' here.
+
+//Result = 3310 Price Rs. 3000 3310 Price Rs. 3000
+
+
+
+
+ 
