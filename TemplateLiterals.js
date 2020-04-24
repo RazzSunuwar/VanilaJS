7b40f857cdd8d174863a99ea9cc33ee75a1db281
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-24 23:16:18 +0545

    Template Literals

--- TemplateLiterals.js
+++ TemplateLiterals.js
@@ -1,9 +1,16 @@
 // Use of Template Literals
 // Use `${}`
 
-let Person = "Ray Sunuwar";
+// let Person = "Ray Sunuwar";
 
-function PersonName(){
-    return `Hello its me, ${Person}. I am from Kathmandu.`;
-}
-console.log(PersonName());
+// function PersonName(){
+//     return `Hello its me, ${Person}. I am from Kathmandu.`;
+// }
+// console.log(PersonName());
+
+
+let a = 50;
+let b = 50;
+console.log(`Sum is ${a+b} and mupliplication is ${a*b}`);
+
+
