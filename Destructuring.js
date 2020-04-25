59692d0f4d9d999b24a3226dafe33b63cfb9b50b
RazzSunuwar <razzkoinch5@gmail.com>
2020-04-25 14:04:30 +0545

    Destructuring Practice

--- Destructuring.js
+++ Destructuring.js
@@ -16,11 +16,38 @@
 
 
 // Assign variables from nested objects
-const nest = {
-    start : { x:5, y:8},
-    end : {x: 6, y: 7}
-}
+// const nest = {
+//     start : { x:5, y:8},
+//     end : {x: 6, y: 7}
+// }
 
-const{ start : { x: startX, y: startY }} = nest;
+// const{ start : { x: startX, y: startY }} = nest;
 
-console.log(startX);    // 5
+// console.log(startX);    // 5
+
+
+
+// Assign Variables from Arrays
+// const [q,, r] = [1, 2, 4, 5, 6];     // 1 comma skip 1 element here
+// console.log(q, r);  // 1 4
+
+
+
+// Rest Operator to Reassign Array Elements
+const [q, r, ...rest] = [1, 2, 3, 4, 5];    //Because of 3 dot rest elements go inside the arrray
+console.log(q, r);  // 1 2
+console.log(rest)   // [3, 4, 5 ]
+
+
+
+// Pass an Object as a Function's Parameters
+// const profileUpdate = (profileData) => {
+//     const { name, age, nationality, location } = profileData;
+//     // Do something with this variables 
+// }
+
+// Change to this
+
+// const profileUpdate = ({ name, age, nationality, location }) => {
+
+// }
