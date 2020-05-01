783e143546c2eb24d42e01b7999e975bd81a5a17
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-01 21:54:13 +0545

    Ternary Operator

---
+++ TernaryOperator.js
@@ -0,0 +1,15 @@
+// Ternary Operator
+//  Condition ? expr1 : expr2
+
+// Regular JavaScript
+let  age = 19;
+if(age >= 18) {
+    console.log("You are an adult!");
+} else {
+    console.log("You are kid");
+};
+// Result: You are an adult!
+
+// Ternary Operator
+console.log((age >= 18) ? "You are an adult!" : "You are a kid.");
+// You are an adult!
