5ef07948af924cb015ad0fc54be52d99c667765e
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-02 17:25:14 +0545

    Standard for loop

---
+++ HigherOrder.js
@@ -0,0 +1,23 @@
+const companies = [
+    {name: "Company 0ne", category: "Finance", start: 1981, end: 2002},
+    {name: "Company Two", category: "Finance", start: 1981, end: 2002},
+    {name: "Company Four", category: "Finance", start: 1981, end: 2002},
+    {name: "Company Five", category: "Finance", start: 1981, end: 2002},
+    {name: "Company Six", category: "Finance", start: 1981, end: 2002},
+    {name: "Company Seven", category: "Finance", start: 1981, end: 2002},
+    {name: "Company Eight", category: "Finance", start: 1981, end: 2002},
+    {name: "Company Nine", category: "Finance", start: 1981, end: 2002}
+];
+
+const ages = [22, 11, 50, 5, 12, 14, 32, 44, 77, 3, 40, 100, 60, 26, 27];
+
+// Standard for loop
+for( let i = 0; i < companies.length; i++) {
+    console.log(companies[i]);
+}
+
+// forEach()
+// filter()
+// map()
+// sort()
+// reduce()
