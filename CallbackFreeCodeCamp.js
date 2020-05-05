2238a0b83ba0497b71d29f88f51628c508339a08
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-05 19:27:22 +0545

    Checked codes

--- CallbackFreeCodeCamp.js
+++ CallbackFreeCodeCamp.js
@@ -1,43 +1,43 @@
-function createQuote(quote, callback){
-  let myQoute = "Like i always say " + quote;
-  callback(myQoute);   //2
-}
+// function createQuote(quote, callback){
+//   let myQoute = "Like i always say " + quote;
+//   callback(myQoute);   //2
+// }
 
-function logQuote(quote){
-  console.log(quote);
-}
+// function logQuote(quote){
+//   console.log(quote);
+// }
 
-createQuote("eat your vegetables!", logQuote);  //1
-// createQuote("eat your vegetables!", function(quote){
-//   console.log(quote)
-// });
+// createQuote("eat your vegetables!", logQuote);  //1
+// // createQuote("eat your vegetables!", function(quote){
+// //   console.log(quote)
+// // });
 
 
 // Example that simulates a request to a server:
-// function serverRequest(query, callback){
-//   setTimeout(function(){
-//     let response = query + "full";
-//     callback(response);
-//   }, 2000)
-// }
+function serverRequest(query, callback){
+  setTimeout(function(){
+    let response = query + "full";
+    callback(response);
+  }, 2000)
+}
 
-// function getResults(results){
-//   console.log("Response to the server: " + results)
-// }
+function getResults(results){
+  console.log("Response to the server: " + results)
+}
 
-// serverRequest("The glass is half ",getResults)
+serverRequest("The glass is half ",getResults)
 
-// // Result: Response to the server: The glass is half full
+// Result: Response to the server: The glass is half full
 
 
 
 // Callback Practice
-function createSentence(quote, callback) {
-  let mysentence = "Helllo world " + quote;
-  callback(mysentence);
-}
+// function createSentence(quote, callback) {
+//   let mysentence = "Helllo world " + quote;
+//   callback(mysentence);
+// }
 
-function logSentence(quote){
-  console.log(quote)
-}
-createSentence("Planet earth is beautiful", logSentence);
+// function logSentence(quote){
+//   console.log(quote)
+// }
+// createSentence("Planet earth is beautiful", logSentence);
