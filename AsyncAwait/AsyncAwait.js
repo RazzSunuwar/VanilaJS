dc0c42c5d87c87e3687a5a2317af831a5f33e0ce
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-09 22:06:19 +0545

    Async await Practice

--- AsyncAwait/AsyncAwait.js
+++ AsyncAwait/AsyncAwait.js
@@ -28,9 +28,11 @@ async function f() {
     let promise = new Promise((resolve, reject) => {
         setTimeout (() => resolve('done!'), 3000)
     });
-    let result = await promise;
+    let result = await promise; //wait until the promise resolves(*)
     alert(result);  //Done in the browser
 }
 f();
+
+
 
 
