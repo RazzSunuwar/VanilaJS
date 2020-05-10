f5519211b9fd4c7c8f39de2388ad6b2e5d8a83ec
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-10 11:48:35 +0545

    Async Await

--- AsyncAwait/AsyncAwait1.js
+++ AsyncAwait/AsyncAwait1.js
@@ -3,24 +3,43 @@ using async/await: */
 //1. We'll need to replace ,then calls with await.
 //2. Also we should make the function async for them to work.
 
-async function showAvatar(){
-    // read our JSON
-    let response = await fetch('/arcticle/promise-channing/user.json')
+// async function showAvatar(){
+//     // read our JSON
+//     let response = await fetch('/arcticle/promise-channing/user.json')
 
-    let user = await response.json();
-    // read github user
-    let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
-    let githubUser = await githubResponse.json();
+//     let user = await response.json();
+//     // read github user
+//     let githubResponse = await fetch(`https://api.github.com/users/${user.name}`);
+//     let githubUser = await githubResponse.json();
 
-    // Show the avatar
-    let img = document.createElement('img');
-    img.src = githubUser.avatar-URL;
-    img.className = "promise-avatar-example";
-    document.body.append(img);
-    // wait untill 3 seconds
-    await new Promise((resolve, reject) => setTimeout(resolve, 3000));
-    img.remove();
-    return githubUser;
-}  
-showAvatar();
+//     // Show the avatar
+//     let img = document.createElement('img');
+//     img.src = githubUser.avatar-URL;
+//     img.className = "promise-avatar-example";
+//     document.body.append(img);
+//     // wait untill 3 seconds
+//     await new Promise((resolve, reject) => setTimeout(resolve, 3000));
+//     img.remove();
+//     return githubUser;
+// }  
+// showAvatar();
+
+
+// Note: await won't work in the top-level code
+/*People who are starting to use await tend to forget the fact that we can't use await
+ in top-level code. For example, this will not work: */
+
+//  syntax error in top-level code
+// let response = await fetch('/article/promise-chaining/user.json');
+// let user = await response.json();
+
+// But we can wrap in into an anonymous async function, like this:
+// (async () => {
+//     let response = await fetch('/article/promise-chaining/user.json');
+//     let user = await response.json();
+    
+// })();
+
+
+ 
 
