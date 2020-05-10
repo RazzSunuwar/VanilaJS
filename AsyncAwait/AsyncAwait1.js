27aa5fbd8b49ca8f7aac45b2509ecc556ed04d5f
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-10 17:36:07 +0545

    AsyncAwait

--- AsyncAwait/AsyncAwait1.js
+++ AsyncAwait/AsyncAwait1.js
@@ -68,3 +68,12 @@ awaits until one of them is called (in the example above it happens in the line 
 and then proceeds with the result.) */
 
 
+
+// async/await and promise.then/catch
+/*When we use async/await, we rarely need .then, because await handles the waiting for us. 
+And we can use a regular try..catch instead of .catch. That’s usually (but not always) more convenient.
+
+But at the top level of the code, when we’re outside any async function, we’re syntactically unable 
+to use await, so it’s a normal practice to add .then/catch to handle the final result or 
+falling-through error, like in the line (*) of the example above. */
+
