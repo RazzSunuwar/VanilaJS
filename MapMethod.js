d3ad6c69c4c0705d747ee47ef49c23c89363c881
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-11 22:18:23 +0545

    Map Method

---
+++ MapMethod.js
@@ -0,0 +1,18 @@
+// map Method
+/* The map() method creates a new array with the results of calling a provided function on every element in the
+calling array. 
+
+map calls a provided callback function once for each element in an array, in order, and returns a new array from
+the results. 
+
+The map() method creates a new array with the results of calling a function for every array element.
+
+The map() method calls the provided function once for each element in an array, in order.
+
+Note: map() does not execute the function for array elements without values.
+
+Note: this method does not change the original array. */
+
+// Syntax:- map(callback(currentValue, index, array), thisValue);
+
+
