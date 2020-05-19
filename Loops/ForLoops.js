b177457b5e4269bf0fe655bb2451b51afe7e5851
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-19 21:19:36 +0545

    For Loops Practice

--- Loops/ForLoops.js
+++ Loops/ForLoops.js
@@ -1,9 +1,19 @@
-// For Loops
-// console.log("JAVASCRIPT FOR LOOP");
-let cars = ["Buggati", "Ferrari", "Lamborgini", "Tesla", "McLaren"];
-let text = "";
+// // For Loops
+// // console.log("JAVASCRIPT FOR LOOP");
+// let cars = ["Buggati", "Ferrari", "Lamborgini", "Tesla", "McLaren"];
+// let text = "";
+// let i;
+// for(i= 0; i< cars.length; i++) {
+//     text += cars[i] + "<br>";
+// }
+// document.getElementById("demo").innerHTML = text;
+
+//self Practice
+
+let bikes = ["Ducati", "Honda", "Yamaha", "Kawasaki", "Suzuki"];
+let text = " ";
 let i;
-for(i= 0; i< cars.length; i++) {
-    text += cars[i] + "<br>";
+for(i=0; i <= bikes.length; i++){
+    text += bikes[i] + "<br>"
+    document.getElementById("demo").innerHTML = text;
 }
-document.getElementById("demo").innerHTML = text;
