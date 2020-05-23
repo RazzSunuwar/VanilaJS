f1a78b54e74d6273ecd34b877bde6c87c81a4d67
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-23 21:39:23 +0545

    Array Destructuring

---
+++ Destructuring/ArrayDestructuring.js
@@ -0,0 +1,8 @@
+let a = ["JavaScript", "TypeScript", "VScript"];
+
+// Destructuring
+let [Programming1,Programming2,Programming3] = a;
+
+console.log(Programming1);  // JavaScript
+console.log(Programming2);  // TypeScript
+console.log(Programming3);  // VScript
