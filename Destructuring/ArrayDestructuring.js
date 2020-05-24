3d9aa76d1d7dc09b9c17b6754c26e958de93766d
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-24 13:10:49 +0545

    Added to subject in destructuring

--- Destructuring/ArrayDestructuring.js
+++ Destructuring/ArrayDestructuring.js
@@ -1,8 +1,15 @@
 let a = ["JavaScript", "TypeScript", "VScript"];
 
 // Destructuring
-let [Programming1,Programming2,Programming3] = a;
+// let [Programming1,Programming2,Programming3] = a;
+
+// console.log(Programming1);  // JavaScript
+// console.log(Programming2);  // TypeScript
+// console.log(Programming3);  // VScript
+
+
+// For two elements only
+let [Programming1,Programming2] = a;
 
 console.log(Programming1);  // JavaScript
 console.log(Programming2);  // TypeScript
-console.log(Programming3);  // VScript
