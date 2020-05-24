8444c9252f3651870dd82738d2554d0d184c2265
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-24 20:49:52 +0545

    Object Destructuring

--- Destructuring/Object/ObjectDestructuring.js
+++ Destructuring/Object/ObjectDestructuring.js
@@ -32,6 +32,10 @@ knows which property of the object we want to assign. */
 // console.log(b); //20
 
 
-let {a=40, b=20} = {a:10};
-console.log(a); //10 default 40 is overwrite by 10
-console.log(b); //20
+// let {a=40, b=20} = {a:10};
+// console.log(a); //10 default 40 is overwrite by 10
+// console.log(b); //20
+
+let{a:y = 40, b:z=20} = {a:10};
+console.log(y)  //10 default value 40 is overwrite by 10
+console.log(z)  //20
