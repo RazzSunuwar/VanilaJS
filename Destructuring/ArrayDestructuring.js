7a978d81c9db25f2a8545b2b9ae599fbfc8135a3
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-24 14:34:12 +0545

    Assign value in elements

--- Destructuring/ArrayDestructuring.js
+++ Destructuring/ArrayDestructuring.js
@@ -1,4 +1,4 @@
-let a = ["JavaScript", "TypeScript", "VScript"];
+// let a = ["JavaScript", "TypeScript", "VScript"];
 
 // Destructuring
 // let [Programming1,Programming2,Programming3] = a;
@@ -17,8 +17,31 @@ let a = ["JavaScript", "TypeScript", "VScript"];
 
 // Leaving element of second 
 
-let [Programming1,,Programming3] = a;
+// let [Programming1,,Programming3] = a;
 
-console.log(Programming1);  // JavaScript
-// console.log(Programming2);  // TypeScript
-console.log(Programming3);  // VScript
+// console.log(Programming1);  // JavaScript
+// // console.log(Programming2);  // TypeScript
+// console.log(Programming3);  // VScript
+
+
+// Another Function
+// let a, b;
+// [a,b] = [10, 20];
+// console.log(a); // 10
+// console.log(b); // 20
+
+
+// var a, b;
+// [a, b=20] = [10];
+// console.log(a); // 10
+// console.log(b); // 20 Write default number 20
+
+
+let a, b;
+[a=40, b=20] = [10];
+console.log(a); // 10 because default value of a is overwrite by 10;
+console.log(b); //20
+
+
+
+
