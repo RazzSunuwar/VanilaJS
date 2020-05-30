a44618f4772129351ec2f2f91f6e63e6e62c4b3f
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-30 13:38:11 +0545

    Practice ForEach

--- ForEach.js
+++ ForEach.js
@@ -1,10 +1,22 @@
-let numbers = [11, 22, 33, 44, 55];
+// let numbers = [11, 22, 33, 44, 55];
 
-numbers.forEach(myFunction);
+// numbers.forEach(myFunction);
 
 
-function myFunction(item, index, arr){
-    arr[index] = item * 2;
-}
+// function myFunction(item, index, arr){
+//     arr[index] = item * 2;
+// }
 
-console.log(numbers);   //[ 22, 44, 66, 88, 110 ]
+// console.log(numbers);   //[ 22, 44, 66, 88, 110 ]
+
+
+
+// Practice
+let  numbers = [1,2, 3, 4, 5, 6, 7];
+
+
+numbers.forEach(function myFunction(item, index, arr){
+    arr[index] = item * 2; 
+});
+
+console.log(numbers);   //[ 2,  4,  6, 8, 10, 12, 14 ]
