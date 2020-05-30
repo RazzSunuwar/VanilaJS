96a2ca8ce0de7a98690c67f2f1ac024de9a700a4
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-30 19:02:36 +0545

    Prototype practice

---
+++ Prototype/Prototype.js
@@ -0,0 +1,72 @@
+// Prototype
+
+// Prototype is parent class or super class or base class. All this classes are same.
+
+
+// Using Prototype
+
+// let Mobile = function(model_no) {
+// 	// Instance Member
+// 	this.model = model_no,
+// 	this.price = 10000
+// }
+
+// let Samsung = new Mobile('S10');
+// let Nokia = new Mobile('Lumia X');
+// // Classname.prototype.key = 'value';
+// Mobile.prototype.color = 'Green';	// Prototype Member
+
+// document.write(Samsung.color);
+// document.write(	Nokia.color);
+// console.log(Samsung);
+// console.log(Nokia);
+
+
+
+// Practice without prototype
+// var Mobile = function(model_no){
+// 	this.model = model_no;
+// 	this.price = 3000;
+// }
+
+// var Samsung = new Mobile('Note');
+// var OnePlus = new Mobile('Touch');
+// Samsung.color = 'White';
+// OnePlus.color = 'Black';
+// console.log(Samsung.color);
+// console.log(OnePlus.color);
+// console.log(Samsung);
+// console.log(OnePlus);
+
+/*
+White
+Black
+Mobile { model: 'Note', price: 3000, color: 'White' }
+Mobile { model: 'Touch', price: 3000, color: 'Black' }
+*/ 
+
+
+
+// Practice with prototype
+var Mobile = function(model_no){
+	this.model = model_no;
+	this.price = 3000;
+}
+
+var Samsung = new Mobile('Note');
+var OnePlus = new Mobile('Touch');
+
+// Classname.prototype.key = 'value';
+Mobile.prototype.color = 'Green';	// Prototype Member
+
+document.write(Samsung.color);
+document.write(OnePlus.color);
+console.log(Samsung);
+console.log(OnePlus);
+
+/*
+Check in browser console
+*/ 
+
+/* Note: JavaScript focus on buttom to top approach we access the data. And most of the
+we used prototype in method.*/ 
