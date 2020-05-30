75e697efdca37d55327f706a883d93f4c6173692
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-30 14:43:18 +0545

    For...in Loop Practice

--- forEachVSforin.js
+++ forEachVSforin.js
@@ -12,15 +12,40 @@ Charles
 
 
 // ForEach
-let names = ["Andy", "Bryan", "Charles"];
+// let names = ["Andy", "Bryan", "Charles"];
 
-names.forEach((item) => {
-    console.log(item)
-});
+// names.forEach((item) => {
+//     console.log(item)
+// });
 /*
 Andy
 Bryan
 Charles
 */
+
+
+
+// for...in
+/* for...in is used to iterate over the enumerable properties of objects. Every properties in an object
+will have an Enumerable value --- if that value is set to true, then the property is Enumerable.*/
+
+
+
+const obj = {
+    a : 1,
+    b : 2,
+    c : 3,
+    d : 4
+}
+
+for (let elem in obj){
+    console.log(obj[elem])
+}
+/*
+1
+2
+3
+4
+*/ 
 
 
