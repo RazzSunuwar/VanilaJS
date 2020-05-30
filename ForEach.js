fcfa9f3c36d08c14d2fdd9cc6c8ec952d429833b
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-30 13:49:22 +0545

    Array String Practice

--- ForEach.js
+++ ForEach.js
@@ -12,11 +12,25 @@
 
 
 // Practice
-let  numbers = [1,2, 3, 4, 5, 6, 7];
+// let  numbers = [1,2, 3, 4, 5, 6, 7];
 
 
-numbers.forEach(function myFunction(item, index, arr){
-    arr[index] = item * 2; 
+// numbers.forEach(function myFunction(item, index, arr){
+//     arr[index] = item * 2; 
+// });
+
+// console.log(numbers);   //[ 2,  4,  6, 8, 10, 12, 14 ]
+
+
+// Practice 2
+let names = ['Arron', 'Brad', 'David'];
+
+names.forEach(function(item){
+    console.log(item)
 });
 
-console.log(numbers);   //[ 2,  4,  6, 8, 10, 12, 14 ]
+/*
+Arron
+Brad
+David
+*/
