0652d8c79c56b0f70601dad3fc3ee301f959ad5e
RazzSunuwar <razzkoinch5@gmail.com>
2020-05-30 14:13:01 +0545

    For Each Practice

--- forEachVSforin.js
+++ forEachVSforin.js
@@ -1,11 +1,26 @@
 // For loop
-let names = ["Andy", "Bryan", "Charles"];
+// let names = ["Andy", "Bryan", "Charles"];
 
-for(i=0; i< names.length; i++){
-    console.log(names[i]);
-}
+// for(i=0; i< names.length; i++){
+//     console.log(names[i]);
+// }
 /*
 Andy
 Bryan
 Charles
 */
+
+
+// ForEach
+let names = ["Andy", "Bryan", "Charles"];
+
+names.forEach((item) => {
+    console.log(item)
+});
+/*
+Andy
+Bryan
+Charles
+*/
+
+
