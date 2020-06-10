abc44773819426c14bfd79699a47eeb9b9285ae6
RazzSunuwar <razzkoinch5@gmail.com>
2020-06-10 21:49:38 +0545

    Practice Return Function

---
+++ Return.js
@@ -0,0 +1,16 @@
+// A return statement may be return Any type data, include arrays and objects.
+// Syntax:- return (variable or expression);
+/* Syntax:- 
+function function_name(para1, para2,...){
+    Block of statement;
+    return (expression);
+}
+*/ 
+
+
+// Example 1
+function add(a, b){
+    return (a + b)
+}
+console.log("Return the answer", add(10, 20));  // Return 30
+// Note Expession will return when the function calls
