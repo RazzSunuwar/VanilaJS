c547159b1b8afcee3464bdcecd47d6b03e146e05
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-02 21:45:38 +0545

    Checked first five number

---
+++ Loops/WhileLoop/main.js
@@ -0,0 +1,26 @@
+// The while loop keeps repeating an action until an associated condition returns false.
+// Syntax:
+/*
+while (test condition)
+{
+    body of the loop;
+    increment/decrement;
+}
+*/
+
+let i = 0;
+while(i<5)
+{
+    document.write(i);
+    i++;
+    document.write("<br>");
+}
+
+//Result: 
+/*
+0
+1
+2
+3
+4
+*/
