0d075d7c1d3518c235014eb87318e3d04f8ad3ce
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-12 21:51:29 +0545

    this in method

---
+++ This/mythis.js
@@ -0,0 +1,11 @@
+// this in method
+
+let person = {
+    firstName : "Brad",
+    lastName : "Watt",
+    fullName : function() {
+        return this.firstName + " " + this.lastName;
+    }
+};
+
+document.getElementById("demo").innerHTML = person.fullName();
