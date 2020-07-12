38c136bfe773884a2a336c575c40bbf0292df62a
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-12 22:00:21 +0545

    this alone

--- This/mythis.js
+++ This/mythis.js
@@ -1,11 +1,13 @@
 // this in method
 
-let person = {
-    firstName : "Brad",
-    lastName : "Watt",
-    fullName : function() {
-        return this.firstName + " " + this.lastName;
-    }
-};
+// let person = {
+//     firstName : "Brad",
+//     lastName : "Watt",
+//     fullName : function() {
+//         return this.firstName + " " + this.lastName;
+//     }
+// };
 
-document.getElementById("demo").innerHTML = person.fullName();
+// document.getElementById("demo").innerHTML = person.fullName();
+
+let x = this;   //Global object
