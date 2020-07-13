b10f4af1b0efaa80f3f54fc215af7f3eb0a22c30
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-13 21:55:33 +0545

    this in a Function

--- This/mythis.js
+++ This/mythis.js
@@ -1,5 +1,4 @@
 // this in method
-
 // let person = {
 //     firstName : "Brad",
 //     lastName : "Watt",
@@ -10,4 +9,10 @@
 
 // document.getElementById("demo").innerHTML = person.fullName();
 
-let x = this;   //Global object
+// let x = this;   //Global object
+
+// this in a function (Default)
+document.getElementById("demo").innerHTML = myFunction();
+function myFunction(){
+    return this;    //Result: [object Window]
+}
