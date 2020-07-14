6f199c2f78466db0fce6f0e31aa58c304dc07500
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-14 21:16:28 +0545

    Object method binding & Explicit Function Binding

--- This/mythis.js
+++ This/mythis.js
@@ -24,8 +24,34 @@
 //     return this;
 // }   //undefined
 
-// this in Event Handlers
-<button onclick="this.style.display='none'">Click to Remove Me !
-</button>
+// // this in Event Handlers
+// <button onclick="this.style.display='none'">Click to Remove Me !
+// </button>
+
+// // Object Method Binding
+// var person = {
+//     firstName: "John",
+//     lastName: "Doe",
+//     fullName: function(){
+//         return this;
+//     }
+// }
+
+// document.getElementById("demo").innerHTML = person.fullName();
+
+var person1 = {
+    fullName: function(){
+        return this.firstName + " " + this.lastName;
+    }
+}
+
+var person2 = {
+    firstName: "John",
+    lastName: "Doe"
+}
+
+var x = person1.fullName.call(person2);
+
+document.getElementById("demo").innerHTML = x;  // John Doe
 
 
