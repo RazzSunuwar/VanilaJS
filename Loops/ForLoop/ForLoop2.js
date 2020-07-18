c2a13ab9b4e9c1ad5fe305a774dd8bcbd3aae4d7
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-18 20:49:33 +0545

    For/In Loop Practice

--- Loops/ForLoop/ForLoop2.js
+++ Loops/ForLoop/ForLoop2.js
@@ -6,10 +6,19 @@
 // }
 
 
-let cars = ["Lamborgini", "Ferrari", "Buggati", "Telsa", "Ford"];
-let text = "";
-for( let i=0; i< cars.length; i++){
-    text += cars[i] + "<br>"
-    document.getElementById("demo").innerHTML = text;
+// let cars = ["Lamborgini", "Ferrari", "Buggati", "Telsa", "Ford"];
+// let text = "";
+// for( let i=0; i< cars.length; i++){
+//     text += cars[i] + "<br>"
+//     document.getElementById("demo").innerHTML = text;
+// }
+
+// for/in loop
+var text ="";
+let person = { fname: "Johh", lname: "Doe", age: 25};
+var x;
+for (x in person){
+    text+= person[x] + " ";
 }
+document.getElementById("demo").innerHTML = text;
 
