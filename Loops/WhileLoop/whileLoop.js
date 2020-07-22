e35b901e1b640f70f5d844219819f5d393de9baa
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-22 22:14:14 +0545

    While lOOP Practice

--- Loops/WhileLoop/whileLoop.js
+++ Loops/WhileLoop/whileLoop.js
@@ -1,20 +1,34 @@
-let text = "";
-let i = 0;
-while (i < 10){
-    text += "<br> The number is " + i;
-    i++;
+// let text = "";
+// let i = 0;
+// while (i < 10){
+//     text += "<br> The number is " + i;
+//     i++;
+// }
+// document.getElementById("demo").innerHTML = text;
+
+// /*
+// The number is 0
+// The number is 1
+// The number is 2
+// The number is 3
+// The number is 4
+// The number is 5
+// The number is 6
+// The number is 7
+// The number is 8
+// The number is 9
+// */
+
+let count = 1;
+while (count < 10){
+    console.log(count); 
+    count += 2;
 }
-document.getElementById("demo").innerHTML = text;
 
 /*
-The number is 0
-The number is 1
-The number is 2
-The number is 3
-The number is 4
-The number is 5
-The number is 6
-The number is 7
-The number is 8
-The number is 9
+1
+3
+5
+7
+9
 */
