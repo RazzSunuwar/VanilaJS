b38d97c7717e0a1d4b6b1683e2adf27953c8bf10
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-26 22:12:33 +0545

    Practice on JavaScript String

---
+++ Strings/string.js
@@ -0,0 +1,8 @@
+// JavaScript String Types
+let str = new String("JavaScript String Types");
+console.log(str.length);    //23
+
+console.log(str.valueOf()); //JavaScript String Types
+console.log(str.toString());    //JavaScript String Types
+console.log(str.toLocaleString());  //JavaScript String Types
+
