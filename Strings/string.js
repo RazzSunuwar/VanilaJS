514e762519104dea1a9efdd8b70f7442c29fe7b7
RazzSunuwar <razzkoinch5@gmail.com>
2020-07-27 22:05:25 +0545

    Practice SubString

--- Strings/string.js
+++ Strings/string.js
@@ -1,15 +1,18 @@
-// JavaScript String Types
-let str = new String("JavaScript String Types");
-console.log(str.length);    //23
+// // JavaScript String Types
+// let str = new String("JavaScript String Types");
+// console.log(str.length);    //23
 
-console.log(str.valueOf()); //JavaScript String Types
-console.log(str.toString());    //JavaScript String Types
-console.log(str.toLocaleString());  //JavaScript String Types
+// console.log(str.valueOf()); //JavaScript String Types
+// console.log(str.toString());    //JavaScript String Types
+// console.log(str.toLocaleString());  //JavaScript String Types
 
 // String manipulation
 
-// 1 Concate
-let firstName = "John";
-let fullName = firstName.concat(" " + "Doe");
-console.log(fullName);
+// // 1 concate()
+// let firstName = "John";
+// let fullName = firstName.concat(" " + "Doe");
+// console.log(fullName);
 
+// 2 substr()
+let str = "JavaScript World !";
+console.log(str.substr(0, 10)); // JavaScript
