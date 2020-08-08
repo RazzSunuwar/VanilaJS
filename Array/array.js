4a0a0d97316749533be0d19d3b53389490687946
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-08 21:33:26 +0545

    Practice push & pop array method

---
+++ Array/array.js
@@ -0,0 +1,15 @@
+// push and pop
+let myArray = ["Ducati", "Harley Davidson", "Honda", "Yamaha", "Suzuki"];
+document.getElementById("demo").innerHTML = myArray;
+//Push()
+// function myFunction() {
+//     myArray.push("Yatri");
+//     document.getElementById("demo").innerHTML = myArray;
+// }
+
+// Pop()
+function myFunction() {
+    myArray.pop();
+    document.getElementById("demo").innerHTML = myArray;
+}
+
