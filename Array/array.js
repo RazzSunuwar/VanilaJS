8664f81b9a41d2b44d56c2af3ae8d425018f882a
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-09 19:57:24 +0545

    Practice Splice method

--- Array/array.js
+++ Array/array.js
@@ -1,15 +1,25 @@
-// push and pop
-let myArray = ["Ducati", "Harley Davidson", "Honda", "Yamaha", "Suzuki"];
-document.getElementById("demo").innerHTML = myArray;
-//Push()
+// // push and pop
+// let myArray = ["Ducati", "Harley Davidson", "Honda", "Yamaha", "Suzuki"];
+// document.getElementById("demo").innerHTML = myArray;
+// //Push()
+// // function myFunction() {
+// //     myArray.push("Yatri");
+// //     document.getElementById("demo").innerHTML = myArray;
+// // }
+
+// // Pop()
 // function myFunction() {
-//     myArray.push("Yatri");
+//     myArray.pop();
 //     document.getElementById("demo").innerHTML = myArray;
 // }
 
-// Pop()
-function myFunction() {
-    myArray.pop();
-    document.getElementById("demo").innerHTML = myArray;
-}
+
+// Queue()
+
+
+// Splice()
+let scores = [0,1,2,3,4,5,6,7,8,9];
+let removeNum = scores.splice(2,7);
+console.log(scores);    // [ 0, 1, 9 ]
+console.log(removeNum); // [ 2, 3, 4, 5, 6, 7, 8 ]
 
