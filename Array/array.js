463fb79907bffa7070cd36f52441546640b17675
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-10 20:47:57 +0545

    IndexOf

--- Array/array.js
+++ Array/array.js
@@ -23,10 +23,14 @@
 // console.log(scores);    // [ 0, 1, 9 ]
 // console.log(removeNum); // [ 2, 3, 4, 5, 6, 7, 8 ]
 
-// slice()
-let Laptops = ["HP", "Dell", "Mac", "ASUS", "MSI", "ACER", "TOSHIBA", "MICROSOFT"];
-let Brands = Laptops.slice(4,6);
-console.log(Brands);    // [ 'MSI', 'ACER' ]
+// // slice()
+// let Laptops = ["HP", "Dell", "Mac", "ASUS", "MSI", "ACER", "TOSHIBA", "MICROSOFT"];
+// let Brands = Laptops.slice(4,6);
+// console.log(Brands);    // [ 'MSI', 'ACER' ]
+
+// indexOf()
+let numbers = [10, 20, 30, 40, 50, 60, 70, 80];
+console.log(numbers.indexOf(50));   //4
 
 
 
