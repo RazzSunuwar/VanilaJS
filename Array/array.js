e82274ffd4b0e1c98ac19eb44d87681b0ba643de
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-10 20:29:25 +0545

    Practice slice

--- Array/array.js
+++ Array/array.js
@@ -18,8 +18,15 @@
 
 
 // Splice()
-let scores = [0,1,2,3,4,5,6,7,8,9];
-let removeNum = scores.splice(2,7);
-console.log(scores);    // [ 0, 1, 9 ]
-console.log(removeNum); // [ 2, 3, 4, 5, 6, 7, 8 ]
+// let scores = [0,1,2,3,4,5,6,7,8,9];
+// let removeNum = scores.splice(2,7);
+// console.log(scores);    // [ 0, 1, 9 ]
+// console.log(removeNum); // [ 2, 3, 4, 5, 6, 7, 8 ]
+
+// slice()
+let Laptops = ["HP", "Dell", "Mac", "ASUS", "MSI", "ACER", "TOSHIBA", "MICROSOFT"];
+let Brands = Laptops.slice(4,6);
+console.log(Brands);    // [ 'MSI', 'ACER' ]
+
+
 
