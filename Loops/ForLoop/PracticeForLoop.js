346055fef0e59b56fce59f75cd4feb78e7166d3c
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-22 22:44:54 +0545

    Practice for loop

--- Loops/ForLoop/PracticeForLoop.js
+++ Loops/ForLoop/PracticeForLoop.js
@@ -1,29 +1,41 @@
 // Practice for loop
-const days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
-// for(let i = 0; i < days.length; i++){
-//     const element = days[i];
-//     console.log(element);
+// const days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
+// // for(let i = 0; i < days.length; i++){
+// //     const element = days[i];
+// //     console.log(element);
+// // }
+// // /*
+// // Result:-
+// // Sun
+// // Mon
+// // Tue
+// // Wed
+// // Thr
+// // Fri
+// // Sat
+// // */ 
+
+// for (let i = days.length-1; i >= 0; i--) {
+//     console.log(days[i])
 // }
 // /*
-// Result:-
-// Sun
-// Mon
-// Tue
-// Wed
-// Thr
-// Fri
 // Sat
+// Fri
+// Thr
+// Wed
+// Tue
+// Mon
+// Sun
 // */ 
 
-for (let i = days.length-1; i >= 0; i--) {
-    console.log(days[i])
-}
+const numbers = [1, 2, 3, 4, 5];
+for (i=0; i < numbers.length; i++){
+    console.log(numbers[i]);
+};
 /*
-Sat
-Fri
-Thr
-Wed
-Tue
-Mon
-Sun
+1
+2
+3
+4
+5
 */ 
