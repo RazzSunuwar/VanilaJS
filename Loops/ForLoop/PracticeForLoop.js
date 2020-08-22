8178727495dbd118144e1c1e8379b9a5d586ec52
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-22 22:08:12 +0545

    Practice Simple for loop

---
+++ Loops/ForLoop/PracticeForLoop.js
@@ -0,0 +1,16 @@
+// Practice for loop
+const days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
+for(let i = 0; i < days.length; i++){
+    const element = days[i];
+    console.log(element);
+}
+/*
+Result:-
+Sun
+Mon
+Tue
+Wed
+Thr
+Fri
+Sat
+*/ 
