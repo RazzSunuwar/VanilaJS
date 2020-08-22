f6a3456b2714919dcb265fc5bd50fc856e9d11e7
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-22 22:27:17 +0545

    Reversed the elements in for loop

--- Loops/ForLoop/PracticeForLoop.js
+++ Loops/ForLoop/PracticeForLoop.js
@@ -1,16 +1,29 @@
 // Practice for loop
 const days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"];
-for(let i = 0; i < days.length; i++){
-    const element = days[i];
-    console.log(element);
+// for(let i = 0; i < days.length; i++){
+//     const element = days[i];
+//     console.log(element);
+// }
+// /*
+// Result:-
+// Sun
+// Mon
+// Tue
+// Wed
+// Thr
+// Fri
+// Sat
+// */ 
+
+for (let i = days.length-1; i >= 0; i--) {
+    console.log(days[i])
 }
 /*
-Result:-
-Sun
-Mon
-Tue
-Wed
-Thr
-Fri
 Sat
+Fri
+Thr
+Wed
+Tue
+Mon
+Sun
 */ 
