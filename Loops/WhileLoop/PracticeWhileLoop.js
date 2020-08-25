db8d3f03e6915e4f9be4b5856dbe0fca27cee162
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-25 21:21:13 +0545

    Practice while loop

--- Loops/WhileLoop/PracticeWhileLoop.js
+++ Loops/WhileLoop/PracticeWhileLoop.js
@@ -1,14 +1,29 @@
-var i = 0;
-while (i < 5)
-{
+// var i = 0;
+// while (i < 5)
+// {
+//     console.log(i);
+//     i++;
+// }
+
+// /*
+// 0
+// 1
+// 2
+// 3
+// 4
+// */
+
+let i = 0;
+while (true){
+    if(i == 5)
+    break;
     console.log(i);
     i++;
 }
-
 /*
 0
 1
 2
 3
 4
-*/
+*/ 
