2b3f9aa6e40bfee095f1a4a4270647c8b6716698
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-25 22:22:15 +0545

    Practice do while loop

--- Loops/WhileLoop/doWhileLoop.js
+++ Loops/WhileLoop/doWhileLoop.js
@@ -9,11 +9,27 @@
 // console.log(result); // 12345
 
 
-let result = "";
+// let result = "";
+
+// let i = 0;
+// do {
+//     result += i;
+//     i++;
+// } while(i < 5);
+// console.log(result);
 
 let i = 0;
+
 do {
-    result += i;
+    if(i == 5)
+    break;
+    console.log(i);
     i++;
-} while(i < 5);
-console.log(result);
+} while(true);
+/*
+0
+1
+2
+3
+4
+*/
