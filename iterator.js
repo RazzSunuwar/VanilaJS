f565393f3fe10fc3f5cfb701cbadebc30d4576d3
RazzSunuwar <razzkoinch5@gmail.com>
2020-08-31 21:30:10 +0545

    Modified the codes

--- iterator.js
+++ iterator.js
@@ -1,3 +1,4 @@
+// Iterable
 let myNumber = new Set([1,2,3,3,4,5]);
 for(let keys of myNumber){
     console.log(keys);
@@ -10,3 +11,12 @@ for(let keys of myNumber){
 4
 5
 */ 
+
+// Iterator
+let myArray = [1,2,3,4,5,6,7];
+let iterator = myArray[Symbol.iterator]();
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
+console.log(iterator.next());
