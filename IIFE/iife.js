30042bfbcb767180eeedf41045e7bee3bf8d0385
RazzSunuwar <razzkoinch5@gmail.com>
2020-09-16 22:56:08 +0545

    iife function

--- IIFE/iife.js
+++ IIFE/iife.js
@@ -5,9 +5,16 @@
 // document.write(a);  //Value of a will not access because a is called outside of the function.
 
 
-(function(a, b){ 
-    var a = 10;
-    console.log(a + " " + b);
-}) (10, 20);
+// (function(a, b){ 
+//     var a = 10;
+//     console.log(a + " " + b);
+// }) (10, 20);
 
-// 10 20
+// // 10 20
+
+((a, b) => {
+    var a = 10;
+    console.log(a  + " " + b);
+})(10, 20);
+
+// Result: 10, 20
