bb54fe0d49f0b597fcdfd074c7a125f8ab2f93b6
RazzSunuwar <razzkoinch5@gmail.com>
2020-09-26 21:59:30 +0545

    practice forIn loop

--- Loops/ForLoop/forInLoop.js
+++ Loops/ForLoop/forInLoop.js
@@ -1,14 +1,24 @@
-let person = {
-    firstName : "Ramsey",
-    lastName: "Traversy",
-    age: 25
-};
+// let person = {
+//     firstName : "Ramsey",
+//     lastName: "Traversy",
+//     age: 25
+// };
 
-for(let keys in person){
-    console.log(keys + ":" + person[keys]);
+// for(let keys in person){
+//     console.log(keys + ":" + person[keys]);
+// }
+// /*
+// firstName:Ramsey
+// lastName:Traversy
+// age:25
+// */ 
+
+
+let person = {fname: "Brad", lname: "Watt", age: 25}
+let text = "";
+let x;
+for (x in person){
+    text += person[x] + " ";
 }
-/*
-firstName:Ramsey
-lastName:Traversy
-age:25
-*/ 
+console.log(text);
+// Result: Brad Watt 25 
