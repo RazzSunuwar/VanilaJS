39017fbd52e763f712bcab0073768637507f2bd8
RazzSunuwar <razzkoinch5@gmail.com>
2020-09-28 20:24:59 +0545

    practice getter and setter

--- Accessors/getter&setter.js
+++ Accessors/getter&setter.js
@@ -1,27 +1,43 @@
+// let user = {
+//     firstName : "Brad",
+//     lastName : "Watt",
+//     get fullName (){
+//         return (this.firstName + " " + this.lastName)
+//     },
+
+//     set fullName(value){
+//         [this.firstName, this.lastName] = value.split(" ")
+//     }
+// };
+
+// // get fullName is executed with the given value
+// alert(user.fullName);   //Brad Watt
+
+
+// // set fullName is executed with the given vaule
+// user.fullName = "Mosh Watt";
+// alert(user.fullName);   //Mosh Watt
+
+// alert(user.firstName);  // Most
+// alert(user.lastName);   // Watt
+
 let user = {
-    firstName : "Brad",
-    lastName : "Watt",
-    get fullName (){
-        return (this.firstName + " " + this.lastName)
+    get name(){
+        return this._name;
     },
 
-    set fullName(value){
-        [this.firstName, this.lastName] = value.split(" ")
+    set name(value){
+        if (value.length < 4){
+            alert("Name is too short, need at least 4 characters");
+            return;
+        }
+        this._name = value;
     }
 };
 
-// get fullName is executed with the given value
-alert(user.fullName);   //Brad Watt
-
-
-// set fullName is executed with the given vaule
-user.fullName = "Mosh Watt";
-alert(user.fullName);   //Mosh Watt
-
-alert(user.firstName);  // Most
-alert(user.lastName);   // Watt
-
-
+user.name = "Chan";
+alert(user.name);
+user.name = "";
 
 
 
