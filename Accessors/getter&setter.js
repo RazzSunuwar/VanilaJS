3b32b0462a9ec5bc25af2869e8ad70574648e1f8
RazzSunuwar <razzkoinch5@gmail.com>
2020-09-30 23:03:53 +0545

    practice getter and setter

--- Accessors/getter&setter.js
+++ Accessors/getter&setter.js
@@ -21,23 +21,42 @@
 // alert(user.firstName);  // Most
 // alert(user.lastName);   // Watt
 
+// let user = {
+//     get name(){
+//         return this._name;
+//     },
+
+//     set name(value){
+//         if (value.length < 4){
+//             alert("Name is too short, need at least 4 characters");
+//             return;
+//         }
+//         this._name = value;
+//     }
+// };
+
+// user.name = "Chan";
+// alert(user.name);
+// user.name = "";
+
 let user = {
     get name(){
         return this._name;
     },
 
     set name(value){
-        if (value.length < 4){
+        if (value.length < 4) {
             alert("Name is too short, need at least 4 characters");
             return;
         }
         this._name = value;
     }
 };
+user.name = "Brad";
+alert(user.name);   //Brad
 
-user.name = "Chan";
-alert(user.name);
-user.name = "";
+// user.name= "";  // Name is too short...
+
 
 
 
