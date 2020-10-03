3fb9240c91e801f7c00833dde27d5cb52e1c7893
RazzSunuwar <razzkoinch5@gmail.com>
2020-10-03 16:41:35 +0545

    Practice Callback

--- Callback/myPractice.js
+++ Callback/myPractice.js
@@ -1,9 +1,32 @@
-let numbers = [1,2,3,4,5,6,7,8,9,0];
+// let numbers = [1,2,3,4,5,6,7,8,9,0];
 
-function isOddNumber(number){
-    return number % 2;
-};
+// // function isOddNumber(number){
+// //     return number % 2;
+// // };
 
-let oddNumbers = numbers.filter(isOddNumber);
-console.log(oddNumbers);
-// [ 1, 3, 5, 7, 9 ]
+// // let oddNumbers = numbers.filter(isOddNumber);
+// // console.log(oddNumbers);
+// // // [ 1, 3, 5, 7, 9 ]
+
+// function btnClicked(){
+//     numbers.sort((a, b) => a - b );
+// }
+
+// let btn = document.querySelector("#btn");
+// btn.addEventListener("click", btnClicked);
+
+function download(url, callback){
+    setTimeout(() => {
+        console.log(`Downloading ${url}...`);
+        callback(url)
+    }, 3*1000);
+}
+
+function process(picture){
+    console.log(`Processing ${picture}`);
+}
+
+let url = "https://www.javascripttutorial.net/javascript-callback/";
+
+download(url, process);
+
