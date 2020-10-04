4dd193ee3dc71394c71ab4b61e30b131c0e41a0f
RazzSunuwar <razzkoinch5@gmail.com>
2020-10-04 23:10:32 +0545

    Pactice Project

--- AsyncAwait/async.await.js
+++ AsyncAwait/async.await.js
@@ -1,11 +1,16 @@
 // async/await works well with promise.all
 // When we need to wait for multiple, we can wrap them in promise.all and then await:
 // wait for the array of results
-let results = await Promise.all([
-    fetch(url1),
-    fetch(url2),
+// let results = await Promise.all([
+//     fetch(url1),
+//     fetch(url2),
     
-]);
+// ]);
 
 /*In the case of an error, it propagates as usual, from the failed promise to Promise.all, 
 and then becomes an exception that we can catch using try..catch around the call. */
+
+async function f(){
+    return 1;
+}
+f().then(alert);
