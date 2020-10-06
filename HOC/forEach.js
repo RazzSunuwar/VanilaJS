79c0534a3086e16c6e896aece86b3623eef800d4
RazzSunuwar <razzkoinch5@gmail.com>
2020-10-06 23:42:26 +0545

    practice forEach

--- HOC/forEach.js
+++ HOC/forEach.js
@@ -1,11 +1,23 @@
-let fruits = ["apple", "orange", "cherry"];
-fruits.forEach(myFunction);
-function myFunction(item, index){
-    document.getElementById("demo").innerHTML += index + ":" + item + "<br>";
-}
+// let fruits = ["apple", "orange", "cherry"];
+// fruits.forEach(myFunction);
+// function myFunction(item, index){
+//     document.getElementById("demo").innerHTML += index + ":" + item + "<br>";
+// }
+
+// /*
+// 0:apple
+// 1:orange
+// 2:cherry
+// */
+
+let letters = ['A', 'B', 'C'];
+
+letters.forEach(function(e) {
+    console.log(e);
+});
 
 /*
-0:apple
-1:orange
-2:cherry
-*/
+A
+B
+C
+*/ 
