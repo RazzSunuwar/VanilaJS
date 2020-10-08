8f6dc01851a796634c07d5757e969f5dd67a019f
RazzSunuwar <razzkoinch5@gmail.com>
2020-10-08 21:05:20 +0545

    Practice bind method

--- Methods/bind.js
+++ Methods/bind.js
@@ -17,15 +17,30 @@
 
 // Bind Practice
 
-let Person = {
-  firstName: "Daniel",
-  lastName: "Dyhl"
+// let Person = {
+//   firstName: "Daniel",
+//   lastName: "Dyhl"
+// };
+
+// let PersonFunction = function(a) {
+//   return "Hello"+" "+this.firstName+" "+"Welcome"+" "+"to "+a;
+// };
+
+// let bound = PersonFunction.bind(Person);
+// // console.dir(bound);
+// console.log(bound("Nepal"));
+
+
+let Name = {
+  firstName : "Brad",
+  lastName : "Watt"
 };
 
-let PersonFunction = function(a) {
-  return "Hello"+" "+this.firstName+" "+"Welcome"+" "+"to "+a;
+let Person = function(data){
+  return "Hello"+ " " + this.firstName+ " " + this.lastName + " " + "Welcome" +" "+ "to" + " " + data;
 };
 
-let bound = PersonFunction.bind(Person);
-console.dir(bound);
-console.log(bound("Nepal"));
+let bound = Person.bind(Name);
+// console.dir(bound);
+console.log(bound("This World"));
+// Hello Brad Watt Welcome to This World
