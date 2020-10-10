fcf412ee5ef667b311cbf7f6eff50ab7e58f95a7
RazzSunuwar <razzkoinch5@gmail.com>
2020-10-10 21:11:27 +0545

    Practice getElementsByName

--- DOM/elementById.js
+++ DOM/elementById.js
@@ -1,3 +1,5 @@
-const element = document.getElementById("root");
-console.log(element);
-// Shows in browser while server is on: JavaScript is World's most used programming language
+// const element = document.getElementById("root");
+// console.log(element);
+// // Shows in browser while server is on: JavaScript is World's most used programming language
+
+
