e9fd4a3080ef4834014da9d5cbb84734f9d9f8e7
RazzSunuwar <razzkoinch5@gmail.com>
2020-10-24 09:33:02 +0545

    Practice Rest Operation

--- RestSpread/Rest.js
+++ RestSpread/Rest.js
@@ -1,11 +1,21 @@
+// function sum(...args){
+//     let total = 0;
+//     for(const a of args){
+//         total += a;
+//     };
+//     return total;
+// };
+
+// console.log(sum(1,2,3));
+
+// // Result: 6
+
 function sum(...args){
-    let total = 0;
-    for(const a of args){
-        total += a;
-    };
-    return total;
+    return args.filter(e => typeof e === "number")
+    .reduce((prev, curr) => prev + curr);
 };
 
-console.log(sum(1,2,3));
+let result = sum(10, "Hi", null, undefined, 20);
+console.log(result);
 
-// Result: 6
+// Result: 30
