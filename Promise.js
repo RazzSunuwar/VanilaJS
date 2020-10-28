55859e53d1dba3437915ef5e98a7848c82a72b7d
RazzSunuwar <razzkoinch5@gmail.com>
2020-10-28 20:45:12 +0545

    Practice promise with callback

--- Promise.js
+++ Promise.js
@@ -1,15 +1,34 @@
-let completed = true;
+// let completed = true;
 
-let PracticeJS = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        if(completed){
-            resolve("I have completed JavaScript.");
-        } else{
-            reject("I haven't completed learning JS yet.");
-        };
+// let PracticeJS = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         if(completed){
+//             resolve("I have completed JavaScript.");
+//         } else{
+//             reject("I haven't completed learning JS yet.");
+//         };
         
-    }, 3 * 1000);
-});
-console.log("Result: ", PracticeJS);
+//     }, 3 * 1000);
+// });
+// console.log("Result: ", PracticeJS);
 
-// Result: Check in browser console 
+// // Result: Check in browser console 
+
+function makePromise(completed){
+    return new Promise(function(resolve, reject){
+        setTimeout(() => {
+            if(completed){
+                resolve("I have completed Learning JavaScript.");
+            } else {
+                reject("I haven't completed learning JavaScript yet.");
+            };
+        }, 3 * 1000);
+    });
+};
+
+let learnJavaScript = makePromise(true);
+
+learnJavaScript.then(
+    success => console.log(success),
+    reason => console.log(reason)
+);
