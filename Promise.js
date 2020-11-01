3a1669c1460c19c15c0707fbca9ea168fc41d8d8
RazzSunuwar <razzkoinch5@gmail.com>
2020-11-01 21:55:50 +0545

    Practice Project

--- Promise.js
+++ Promise.js
@@ -57,33 +57,50 @@
 
 
 
-function load(url) {
-    return new Promise(function(resolve, reject){
-        const request = new XMLHttpRequest();
+// function load(url) {
+//     return new Promise(function(resolve, reject){
+//         const request = new XMLHttpRequest();
         
-        request.onreadystatechange = function(e){
-            if (this.readyState === 4) {
-                if(this.status === 200){
-                    resolve(this.response);
-                } else {
-                    reject(this.status);
-                }
-            }
-        }
-        request.open("GET", url, true);
-        request.send();
-    });
-};
+//         request.onreadystatechange = function(e){
+//             if (this.readyState === 4) {
+//                 if(this.status === 200){
+//                     resolve(this.response);
+//                 } else {
+//                     reject(this.status);
+//                 }
+//             }
+//         }
+//         request.open("GET", url, true);
+//         request.send();
+//     });
+// };
 
-const btn = document.querySelector("#btnGet");
-const msg= document.querySelector("#message");
-btn.onclick = function(){
-    load("https://www.javascripttutorial.net/sample/promise/api.json")
-    .then(
-        response => {
-            const result = JSON.parse(response);
-            msg.innerHTML = result.message;
-        },
-        error => msg.innerHTML = `Error getting the message, HTTP status: ${error}`
-    );
-};
+// const btn = document.querySelector("#btnGet");
+// const msg= document.querySelector("#message");
+// btn.onclick = function(){
+//     load("https://www.javascripttutorial.net/sample/promise/api.json")
+//     .then(
+//         response => {
+//             const result = JSON.parse(response);
+//             msg.innerHTML = result.message;
+//         },
+//         error => msg.innerHTML = `Error getting the message, HTTP status: ${error}`
+//     );
+// };
+
+let p = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve(10);
+    }, 3 * 100);
+});
+
+p.then((result) => {
+    console.log(result);
+    return result * 2;
+}).then((result) => {
+    console.log(result);
+});
+
+// Result:
+// 10
+// 20
