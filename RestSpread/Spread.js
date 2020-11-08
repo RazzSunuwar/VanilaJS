8c09b5e3653e782f8789be01e83696b82d57257b
RazzSunuwar <razzkoinch5@gmail.com>
2020-11-08 22:18:10 +0545

    Practice Spread Operator

--- RestSpread/Spread.js
+++ RestSpread/Spread.js
@@ -1,6 +1,12 @@
-// Spread: Increase 
-let a = [1,2,3,4];
-let b = [...a, 5,6,7];
-console.log(b);
+// // Spread: Increase 
+// let a = [1,2,3,4];
+// let b = [...a, 5,6,7];
+// console.log(b);
 
-// [ 1, 2, 3, 4, 5, 6, 7 ]
+// // [ 1, 2, 3, 4, 5, 6, 7 ]
+
+const odd = [1, 3, 5];
+const combined = [2, 4, 6, ...odd];
+console.log(combined);
+// Result: [ 2, 4, 6, 1, 3, 5 ]
+
