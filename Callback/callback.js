845d632a33b124176b235d5c79575028fe1ac511
RazzSunuwar <razzkoinch5@gmail.com>
2020-11-16 20:29:08 +0545

    add arrow function

--- Callback/callback.js
+++ Callback/callback.js
@@ -119,16 +119,17 @@
 
 
 //                        OR
-let add = function(a,b){
+
+let add = (a,b) => {
   return a + b;
-}
+};
 
-let multiply = function(a,b){
-  return a*b;
-}
+let multiply = (a,b) => {
+  return a * b;
+};
 
-let calc = function(num1, num2, callback){
+let calc = (num1, num2, callback) => {
   return callback(num1, num2);
-}
+};
 
 console.log(calc(2, 3, multiply));
