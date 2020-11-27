2e264ca1084729f7f616a75fe0def5955e079f65
RazzSunuwar <razzkoinch5@gmail.com>
2020-11-27 21:35:27 +0545

    Practice array destructuring

--- Destructuring/Array/ArrayDestructuring2.js
+++ Destructuring/Array/ArrayDestructuring2.js
@@ -9,13 +9,31 @@
 // show(arr);
 
 
-// With rest operator
-let arr = [10,20,30,40,50,60,70,80];
+// // With rest operator
+// let arr = [10,20,30,40,50,60,70,80];
 
-function show([value1, value2, value3, ...rest]){
-    console.log(value1);    //10
-    console.log(value2);    //20
-    console.log(value3);    //30
-    console.log(rest);      //[40, 50, 60, 70, 80]
+// function show([value1, value2, value3, ...rest]){
+//     console.log(value1);    //10
+//     console.log(value2);    //20
+//     console.log(value3);    //30
+//     console.log(rest);      //[40, 50, 60, 70, 80]
+// }
+// show(arr);
+
+// Practice 
+
+let numbers = [1, 2, 3, 4, 5];
+
+function practiceNumber([number1, number2, number3, ...rest]){
+    console.log(number1);
+    console.log(number2);
+    console.log(number3);
+    console.log(rest)
 }
-show(arr);
+
+practiceNumber(numbers);
+
+// Result: 1
+/* 2
+ 3
+ [ 4, 5 ] */
