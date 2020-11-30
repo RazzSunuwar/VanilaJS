43938e0be882ff2aec70235821679e5408797fa5
RazzSunuwar <razzkoinch5@gmail.com>
2020-11-30 20:29:04 +0545

    Practice Destructuring

--- EcmaScript.js
+++ EcmaScript.js
@@ -1,15 +1,33 @@
 // Function Object Destructuring
 
-const personOne = {
-    name: 'Albert',
-    age: 26,
+// const personOne = {
+//     name: 'Albert',
+//     age: 26,
+//     address: {
+//         location: 'USA',
+//         state: 'California'
+//     }
+// };
+
+// const prinUser = ({ name, age }) => {
+//     console.log(`Name is ${name}. Age is ${age}`);
+// }
+// prinUser(personOne);    // Name is Albert. Age is 26.
+
+
+const PersonDetails = {
+    name: "Brad",
+    age: 23,
     address: {
-        location: 'USA',
-        state: 'California'
+        location: "street-45 New York",
+        country: "USA" 
     }
 };
 
-const prinUser = ({ name, age }) => {
-    console.log(`Name is ${name}. Age is ${age}`);
-}
-prinUser(personOne);    // Name is Albert. Age is 26.
+const PrintDetails = ({ name, address}) => {
+    console.log(`Name is ${name}. Address is ${address.location}`)
+};
+
+PrintDetails(PersonDetails);
+
+// Result: Name is Brad. Address is street-45 New York
