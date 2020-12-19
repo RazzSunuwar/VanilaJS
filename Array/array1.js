a13e6aa9f52deaa50d53099018cb267107ccdeb9
RazzSunuwar <razzkoinch5@gmail.com>
2020-12-19 23:59:34 +0545

    Practice forEach

--- Array/array1.js
+++ Array/array1.js
@@ -1,6 +1,13 @@
-let myArray = ["Good", "Better", "Best"];
+// let myArray = ["Good", "Better", "Best"];
 
-let newArray = myArray.splice(1,1);
-console.log(newArray);
-// Result: ["Better"]
+
+// let newArray = myArray.splice(1,1);
+// console.log(newArray);
+
+// // Result: ["Better"]
+
+let myArray1 = ["Apple", "Berry", "Coconut", "Orange"];
+myArray1.forEach(function(index, item){
+    document.getElementById("demo").innerHTML += index + ":"+ item + "<br>"
+});
 
