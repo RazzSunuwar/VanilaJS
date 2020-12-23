5a8c7f02279c70d7df9799e34c16f542d58507bf
RazzSunuwar <razzkoinch5@gmail.com>
2020-12-23 23:34:15 +0545

    practice switch case

--- Switch/main.js
+++ Switch/main.js
@@ -1,25 +1,53 @@
+// let day;
+// switch (new Date().getDay()) {
+//     case 0:
+//         day = "Sunday";
+//     break;
+//     case 1:
+//         day = "Monday";
+//     break;
+//     case 2:
+//         day = "Tuesday";
+//     break;
+//     case 3:
+//         day = "Wednesday";
+//     break;
+//     case 4:
+//         day = "Thursday";
+//     break;
+//     case 5:
+//         day = "Friday";
+//     break;
+//     case 6:
+//         day = "Saturday";
+//     break;
+// }
+// document.getElementById("demo").innerHTML = "Today is " + day;
+
 let day;
-switch (new Date().getDay()) {
+switch (new Date().getDay()){
     case 0:
-        day = "Sunday";
+        day = "First Day"
     break;
     case 1:
-        day = "Monday";
+        day = "Second Day"
     break;
     case 2:
-        day = "Tuesday";
+        day = "Third Day"
     break;
     case 3:
-        day = "Wednesday";
+        day = "Fourth Day"
     break;
     case 4:
-        day = "Thursday";
+        day = "Fifth Day"
     break;
     case 5:
-        day = "Friday";
+        day = "Sixth Day"
     break;
     case 6:
-        day = "Saturday";
+        day = "Seventh Day"
     break;
-}
+};
+
 document.getElementById("demo").innerHTML = "Today is " + day;
+// Result in browser console: Today is Fourth Day
