20de27ca1eaf27d606271414e84cba5a4f7c7028
RazzSunuwar <razzkoinch5@gmail.com>
2020-12-29 21:53:07 +0545

    practice callback

--- Callback/callback2.js
+++ Callback/callback2.js
@@ -1,12 +1,28 @@
-function myResult(result){
-    console.log(result);
-};
+// function myResult(result){
+//     console.log(result);
+// };
 
-function total(a, b){
-    let sum = a + b;
-    myResult(sum);
-};
+// function total(a, b){
+//     let sum = a + b;
+//     myResult(sum);
+// };
 
-total(5,5);
+// total(5,5);
 
 // Reult: 10
+
+// JavaScript Callback
+
+function myResult(result){
+    console.log(result)
+};
+
+function total(a, b, callback){
+    let sum = a + b;
+    callback(sum);
+};
+
+total(5,5, myResult);
+
+// Result: 10
+
