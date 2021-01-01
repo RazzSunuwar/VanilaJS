df588aa2e6367c339d150722d79072ffce956018
RazzSunuwar <razzkoinch5@gmail.com>
2021-01-01 21:51:28 +0545

    Practice sorting

--- Array/array1.js
+++ Array/array1.js
@@ -6,8 +6,14 @@
 
 // // Result: ["Better"]
 
-let myArray1 = ["Apple", "Berry", "Coconut", "Orange"];
-myArray1.forEach(function(index, item){
-    document.getElementById("demo").innerHTML += index + ":"+ item + "<br>"
-});
+// let myArray1 = ["Apple", "Berry", "Coconut", "Orange"];
+// myArray1.forEach(function(index, item){
+ //   document.getElementById("demo").innerHTML += index + ":"+ item + "<br>"
+// });
+
+let fruits = ["Grapes", "Mango", "Apple", "Papaya"];
+let result = fruits.sort();
+console.log(result);
+
+// [ 'Apple', 'Grapes', 'Mango', 'Papaya' ]
 
