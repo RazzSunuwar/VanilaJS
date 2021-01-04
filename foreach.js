8e362f1d39b7bd836c182ed4be2d1aedc668a060
RazzSunuwar <razzkoinch5@gmail.com>
2021-01-04 22:55:58 +0545

    Practice foreach method

--- foreach.js
+++ foreach.js
@@ -1,14 +1,24 @@
-let numbers = [1, 2, 3, 4, 5];
-numbers.forEach(foreachfunction);
+// Practice 1
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(foreachfunction);
 
-function foreachfunction(currentValue, index, arr){
-    console.log('a[' + currentValue + '] = ' + index)
-};
+// function foreachfunction(currentValue, index, arr){
+//     console.log('a[' + currentValue + '] = ' + index);
+// };
 
-/* 
-a[1] = 0
-a[2] = 1
-a[3] = 2
-a[4] = 3
-a[5] = 4
-*/ 
+// /* 
+// a[1] = 0
+// a[2] = 1
+// a[3] = 2
+// a[4] = 3
+// a[5] = 4
+// */ 
+
+// Practice 2
+let numbers = [1, 2, 3, 4, 5, 6];
+numbers.forEach((item, index, arr) => {
+    arr[index] = item + 2;
+});
+
+console.log(numbers);
+// Result: [ 3, 4, 5, 6, 7, 8 ]
