eb0703367f7920b86f7403503519ed3becbd42f5
RazzSunuwar <razzkoinch5@gmail.com>
2021-01-06 22:14:06 +0545

    Practice reduce method

--- reduce.js
+++ reduce.js
@@ -19,27 +19,59 @@
 
 
 
-// function callback(accumulator, vaule){
-//     if(accumulator > vaule){
+// function callback(accumulator, value){
+//     if(accumulator > value){
 //         return accumulator;
 //     } else {
-//         return vaule;
+//         return value;
 //     }
 // };
 // console.log("Max number in array is", maxNumber);
 // // Max number in array is 10
 
-let numbers = [1, 2, 3, 6, 5, 7, 8];
+// // Practice 3
+// let numbers = [1, 2, 3, 6, 5, 7, 8];
 
-let Greatest = numbers.reduce(callback, 0);
+// let Greatest = numbers.reduce(callback, 0);
 
-function callback(accumulator, value){
-    if (accumulator % 2 == 4){
-        return accumulator;
-    } else {
-        return value;
+// function callback(accumulator, value){
+//     if (accumulator % 2 == 4){
+//         return accumulator;
+//     } else {
+//         return value;
+//     }
+// };
+
+// console.log(Greatest);
+// // Result: 8
+
+// Practice 4
+let store = [
+    {
+        product: "Laptop",
+        value: 150000,
+        count: 5
+    },
+    {
+        products: "PC",
+        value: 50000,
+        count: 5
+    },
+    {
+        product: "Mobile",
+        value: 10000,
+        count: 3
     }
+];
+
+// Task: Find the total value
+
+let totalValue = store.reduce(callback, 0);
+
+function callback(accumulator, item){
+    return accumulator + item.value * item.count
 };
 
-console.log(Greatest);
-// Result: 8
+console.log(totalValue);
+
+// Result: 1030000
