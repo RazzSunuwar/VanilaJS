e934582b86a8e3df82faa5e08fab07b562b87994
RazzSunuwar <razzkoinch5@gmail.com>
2021-01-18 22:16:49 +0545

    Practice on while loop

---
+++ while.js
@@ -0,0 +1,15 @@
+// Syntax
+/*
+while(condition) {
+    code block to be executed
+}
+*/ 
+
+let words = "";
+let i = 0;
+while(i < 5){
+    words += +i;
+    i++;
+};
+console.log(words);
+// Result: 01234
