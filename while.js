c29350b248ca93d2c038d4463c93b9163ebf9d01
RazzSunuwar <razzkoinch5@gmail.com>
2021-01-20 05:56:03 +0545

    Practice while loop

--- while.js
+++ while.js
@@ -14,13 +14,28 @@ while(condition) {
 // console.log(words);
 // // Result: 01234
 
-let cars = ["BMW", "TESLA", "LAMBORGINI", "FERRARI", "BUGGATI"];
-let text = "";
+// let cars = ["BMW", "TESLA", "LAMBORGINI", "FERRARI", "BUGGATI"];
+// let text = "";
+// let i = 0;
+// while(cars[i]){
+//     text += cars[i] + "<br>";
+//     i++;
+// };
+
+// document.getElementById("demo") = text;
+
+let cars = ["BMW", "Volvo", "Saab", "Ford"];
 let i = 0;
-while(cars[i]){
-    text += cars[i] + "<br>";
-    i++;
-};
+let text = "";
+while (cars[i]){
+    text += cars[i] + "<br>"
+}
 
-document.getElementById("demo") = text;
+document.getElementById("demo").innerHTML = text;
 
+/*
+BMW,
+Volvo,
+Saab,
+Ford
+*/ 
