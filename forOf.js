9ba1a9d85cc7564920d94089a057aaf7fdba098e
RazzSunuwar <razzkoinch5@gmail.com>
2021-01-25 15:29:26 +0545

    Practice forOf loop

--- forOf.js
+++ forOf.js
@@ -1,7 +1,20 @@
-const elements = [1, 2, 3, 4, 5];
+// const elements = [1, 2, 3, 4, 5];
 
-for(const element of elements){
-    console.log(element);
+// for(const element of elements){
+//     console.log(element);
+// };
+
+// //Result: 1, 2, 3, 4, 5
+
+let obj = { a:1, b:2, c:3};
+
+for(const prop in obj){
+    console.log(`obj.${prop} = ${obj[prop]}`);
 };
 
-//Result: 1, 2, 3, 4, 5
+/*
+Result:
+obj.a = 1
+obj.b = 2
+obj.c = 3
+*/
