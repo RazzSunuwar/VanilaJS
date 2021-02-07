03118e2995c842f62fd54563c900164c777ceb94
RazzSunuwar <razzkoinch5@gmail.com>
2021-02-07 23:23:33 +0545

    Practice filter method

--- filter.js
+++ filter.js
@@ -9,13 +9,21 @@
 // // Result: [ 44, 55, 66, 77, 88 ]
 
 // Example: 2
-let numbers = [ 1, 3, 6, 8, 11];
+// let numbers = [ 1, 3, 6, 8, 11];
 
-let lucky = numbers.filter((number) => {
-    return number > 3;
-});
+// let lucky = numbers.filter((number) => {
+//     return number > 3;
+// });
 
-console.log(lucky);
+// console.log(lucky);
 
-// Result: [ 6, 8, 11 ]
+// // Result: [ 6, 8, 11 ]
+
+//Example: 3
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+let result = numbers.filter((number) => ( number >= 4));
+console.log(result);
+// Result: [ 4, 5, 6, 7 ]
+
+
 
