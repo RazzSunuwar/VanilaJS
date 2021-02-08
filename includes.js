0bee568c1f0f895c03ab0cf10ff55de8b0151a4b
RazzSunuwar <razzkoinch5@gmail.com>
2021-02-08 23:08:14 +0545

    Practice includes method

--- includes.js
+++ includes.js
@@ -1,3 +1,8 @@
-let data = "Hello World";
-console.log(data.includes("World"));
-// true
+// let data = "Hello World";
+// console.log(data.includes("World"));
+// // Result: true
+
+let mydata = "Hello People";
+let method = mydata.includes("People");
+console.log(method);
+// Result: true
