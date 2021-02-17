7e87f5fcf868d1b93597d673822c4992dc5618d8
RazzSunuwar <razzkoinch5@gmail.com>
2021-02-17 22:46:52 +0545

    Practice for in loop

--- forIn.js
+++ forIn.js
@@ -1,16 +1,38 @@
-let person = {
-    firstName : "Victory",
-    lastName : "Page",
-    phone: 1245589
-};
+// Practice 1
+// let person = {
+//     firstName : "Victory",
+//     lastName : "Page",
+//     phone: 1245589
+// };
 
-function myFunction(){
-    let text = "";
+// function myFunction(){
+//     let text = "";
+//     let x;
+//     for(x in person){
+//         text += person[x] + " ";
+//     }
+//     console.log(text)
+// };
+
+// myFunction();
+// // Result: Victory Page 1245589
+
+// Practice 2
+const myFunction = () => {
+    let person = {
+        fname: "Brad",
+        lname: "Watt",
+        age: 25
+    };
+
+    let text = " ";
     let x;
     for(x in person){
-        text += person[x] + " ";
-    }
-    console.log(text)
-}
+        text += person[x]+ " ";
+    };
+    console.log(text);
+};
+
 myFunction();
-// Result: Victory Page 1245589
+
+// Result: Brad Watt 25 
