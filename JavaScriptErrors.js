0af918cd243156be2fd3a8d06dbf48bf36830723
RazzSunuwar <razzkoinch5@gmail.com>
2021-02-22 21:41:31 +0545

    Error checking

--- JavaScriptErrors.js
+++ JavaScriptErrors.js
@@ -1,1 +1,15 @@
-
+function myFunction(){
+    let message, x;
+    message = document.getElementById("P01");
+    message.innerHTML = "";
+    x = document.getElementById("demo").value;
+    try {
+        if(x == "") throw "empty";
+        if(isNaN(x))throw "not a number";
+        x = Number(x);
+        if(x < 5) throw "too low";
+        if(x > 10) throw "too high";
+    } catch (err) {
+        message.innerHTML = "Input is " + err;
+    }
+}
